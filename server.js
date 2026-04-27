b894e5726af9a6481225fd5d8869f1b1ffe4ff6a
yohandagita25 <yohandapratiwi8@gmail.com>
2026-04-27 09:06:16 +0700

    fix app DAN SERVER JS

--- server.js
+++ server.js
@@ -4,10 +4,9 @@ const PORT = process.env.PORT || 5000;
 
 // Ini tetap ada agar bisa jalan di lokal
 if (process.env.NODE_ENV !== 'production') {
-  app.listen(PORT, () => {
+  app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
   });
 }
 
-// WAJIB: Ekspor app agar Vercel bisa mengenali rute-rutenya
 module.exports = app;
