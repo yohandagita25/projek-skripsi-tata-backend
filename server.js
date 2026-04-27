aee07a5d9bd92334794846fb26a3c0d53b0f0fa7
yohandagita25 <yohandapratiwi8@gmail.com>
2026-04-27 11:58:35 +0700

    fix server

--- server.js
+++ server.js
@@ -2,11 +2,8 @@ const app = require("./src/app");
 
 const PORT = process.env.PORT || 5000;
 
-// Ini tetap ada agar bisa jalan di lokal
-if (process.env.NODE_ENV !== 'production') {
-  app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 module.exports = app;
