77959f3a37db68308887f8837c0d0759d62c0bef
yohandagita25 <yohandapratiwi8@gmail.com>
2026-04-30 17:07:29 +0700

    fix test

--- src/routes/testRoutes.js
+++ src/routes/testRoutes.js
@@ -4,7 +4,7 @@ const testController = require("../controllers/testController");
 const upload = require("../middleware/uploadMiddleware");
 
 // ✅ Semua rute ini akan memiliki prefix /api/tests di file server.js utama
-router.get("/results/:testType/:courseId", authenticateToken, testController.getDetailedResults);
+router.get("/results/:testType/:courseId", testController.getDetailedResults);
 router.get("/results/:testType/:courseId", testController.getTestResultsByCourse);
 router.post("/upload", upload.single("file"), testController.uploadAndParse);
 router.post("/upload-image", upload.single("image"), testController.uploadImage);
